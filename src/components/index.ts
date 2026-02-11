026a42c1bb3ca0b50039d2e92b9c788d461a09a6
Matéo GUIDI <mguidi@Cordouan.local>
2026-02-11 11:27:32 +0100

    project built

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { MenuScreen } from './MenuScreen.tsx';
-export { GameScreen } from './GameScreen.tsx';
-export { GameHeader } from './GameHeader.tsx';
-export { SudokuGrid } from './SudokuGrid.tsx';
-export { NumberKeypad } from './NumberKeypad.tsx';
-export { WinModal } from './WinModal.tsx';
--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { useBestTimes } from './useBestTimes.ts';
-export { useTimer } from './useTimer.ts';
-export { useSudokuGame } from './useSudokuGame.ts';
